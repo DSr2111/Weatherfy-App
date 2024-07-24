7b649caf49a2a23df384d1aeb4a5e0dd58f0bbc3
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-07-24 18:27:05 -0400

    db update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'sb123'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sb123@localhost:5432/weather_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dsr:Lss4SfvWTPJl8JEkTMGw5ChzJBoom5Df@dpg-cqgnr8lds78s73bjg9ug-a.oregon-postgres.render.com/weather_db_ocxr'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):
